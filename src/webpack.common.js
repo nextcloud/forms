27e545f7b775f16d23efb20bd1a629af2dcb54a4
Roeland Jago Douma <roeland@famdouma.nl>
2019-08-30 21:06:00 +0200

    Move js
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- src/webpack.common.js
+++ src/webpack.common.js
@@ -4,10 +4,10 @@ const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
 	entry: {
-		'forms' : path.join(__dirname, 'src/js/', 'main.js'),
+		'forms' : path.join(__dirname, 'main.js'),
 	},
 	output: {
-		path: path.resolve(__dirname, './js'),
+		path: path.resolve(__dirname, '../js'),
 		publicPath: '/js/',
 		filename: '[name].js'
 	},
