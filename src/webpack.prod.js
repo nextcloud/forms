852b1fc566289d8a52f206b86c6e232673b398a9
Roeland Jago Douma <roeland@famdouma.nl>
2019-09-02 17:18:13 +0200

    Fix eslint errors
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- src/webpack.prod.js
+++ src/webpack.prod.js
@@ -1,7 +1,7 @@
-/*jshint esversion: 6*/
+/** jshint esversion: 6 */
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  mode: 'production'
+	mode: 'production'
 })
