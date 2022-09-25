a41b54d4a449ee828e8a878dec3637a328d5f45a
Jonas Rittershofer <jotoeri@users.noreply.github.com>
2022-09-25 21:55:59 +0200

    Fix upgrading
    Signed-off-by: Jonas Rittershofer <jotoeri@users.noreply.github.com>

--- lib/Migration/Version030000Date20220926200602.php
+++ lib/Migration/Version030000Date20220926200602.php
@@ -1,12 +1,13 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * @copyright Copyright (c) 2022 Jonas Rittershofer <jotoeri@users.noreply.github.com>
  *
  * @author Jonas Rittershofer <jotoeri@users.noreply.github.com>
  *
- * @license AGPL-3.0-or-later
+ * @license GNU AGPL version 3 or any later version
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -15,43 +16,45 @@ declare(strict_types=1);
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 namespace OCA\Forms\Migration;
 
 use Closure;
 use OCP\DB\ISchemaWrapper;
-use OCP\DB\Types;
+use OCP\IDBConnection;
 use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
 
-class Version030000Date20220414203511 extends SimpleMigrationStep {
+class Version030000Date20220926200602 extends SimpleMigrationStep {
+	/** @var IDBConnection */
+	protected $connection;
+
+	/**
+	 * @param IDBConnection $connection
+	 */
+	public function __construct(IDBConnection $connection) {
+		$this->connection = $connection;
+	}
+
 	/**
 	 * @param IOutput $output
 	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
 	 * @param array $options
-	 * @return null|ISchemaWrapper
 	 */
-	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
-		/** @var ISchemaWrapper $schema */
-		$schema = $schemaClosure();
-		$table = $schema->getTable('forms_v2_questions');
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+		$qb = $this->connection->getQueryBuilder();
 
-		if (!$table->hasColumn('description')) {
-			$table->addColumn('description', Types::TEXT, [
-				'notnull' => false,
-				'length' => 4096,
-				'default' => '',
-			]);
-
-			return $schema;
-		}
-		
-		return null;
+		// Set old questions with empty description.
+		$qb->update('forms_v2_questions')
+			->set('description', $qb->expr()->literal(''))
+			->where($qb->expr()->isNull('description'))
+			->executeStatement();
 	}
 }
