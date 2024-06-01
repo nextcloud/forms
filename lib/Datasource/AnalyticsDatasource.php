4ae9b809f01ff5caf9b3ee0630c93dd3c1a2ce0e
Rello <Rello@users.noreply.github.com>
2024-06-01 17:28:14 +0200

    Create AnalyticsDatasource.php
    Signed-off-by: Rello <Rello@users.noreply.github.com>

---
+++ lib/Datasource/AnalyticsDatasource.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Analytics data source
+ * Report Table App data with the Analytics App
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the LICENSE.md file.
+ *
+ * @author Marcel Scherello <analytics@scherello.de>
+ */
+
+namespace OCA\Forms\Datasource;
+
+use OCA\Analytics\Datasource\IDatasource;
+use OCP\IL10N;
+use Psr\Log\LoggerInterface;
+use OCA\Forms\Db\FormMapper;
+use OCA\Forms\Service\FormsService;
+use OCA\Forms\Service\SubmissionService;
+
+class AnalyticsDatasource implements IDatasource
+{
+    private LoggerInterface $logger;
+    private IL10N $l10n;
+    protected ?string $userId;
+
+    public function __construct(
+        IL10N                     $l10n,
+        LoggerInterface           $logger,
+        ?string                   $userId,
+        private FormMapper        $formMapper,
+        private FormsService      $formsService,
+        private SubmissionService $submissionService
+    )
+    {
+        $this->l10n = $l10n;
+        $this->logger = $logger;
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return string Display Name of the datasource
+     */
+    public function getName(): string
+    {
+        return $this->l10n->t('Nextcloud Forms');
+    }
+
+    /**
+     * @return int 2 digit unique datasource id
+     */
+    public function getId(): int
+    {
+        return 66;
+    }
+
+    /**
+     * available options of the data source
+     *
+     * return needs to be an array and can consist of many fields.
+     * every field needs to provide the following format
+     *  id          *mandatory*     = name of the option for the readData
+     *  name        *mandatory*     = displayed name of the input field in the UI
+     *  type        *optional*      = 'tf' to create a dropdown. Values need to be provided in the placeholder separated with "/".
+     *  placeholder *mandatory*     = help text for the input field in the UI
+     *                                for type=tf:
+     *                                  e.g. "true/false"
+     *                                  if value/text pairs are required for the dropdown/option, the values need to be separated with "-" in addition.
+     *                                  e.g. "eq-equal/gt-greater"
+     *                                  to avoid translation of the technical strings, separate them
+     *                                  'true-' - $this->l10n->t('Yes').'/false-'.$this->l10n->t('No')
+     *
+     *  example:
+     *  {['id' => 'datatype', 'name' => 'Type of Data', 'type' => 'tf', 'placeholder' => 'adaptation/absolute']}
+     *
+     * @return array
+     */
+    public function getTemplate(): array
+    {
+        $formsString = '';
+        $questionString = '';
+        $template = [];
+
+        $forms = $this->formMapper->findAllByOwnerId($this->userId);
+        foreach ($forms as $form) {
+            $formsString = $formsString . $form->getId() . '-' . $form->getTitle() . '/';
+            /* Questions are not yet required
+            $questions = $this->formsService->getQuestions($form->getId());
+            foreach ($questions as $question) {
+                $questionString = $questionString . $question['id'] . '-' . $question['text'] . '/';
+            }*/
+        }
+
+        // add the tables to a dropdown in the data source settings
+        $template[] = ['id' => 'formId', 'name' => $this->l10n->t('Select form'), 'type' => 'tf', 'placeholder' => $formsString];
+        $template[] = ['id' => 'timestamp', 'name' => $this->l10n->t('Timestamp of data load'), 'placeholder' => 'false-' . $this->l10n->t('No') . '/true-' . $this->l10n->t('Yes'), 'type' => 'tf'];
+        return $template;
+    }
+
+    /**
+     * Read the Data
+     *
+     * return needs to be an array
+     *  [
+     *      'header' => $header,  //array('column header 1', 'column header 2','column header 3')
+     *      'dimensions' => array_slice($header, 0, count($header) - 1),
+     *      'data' => $data,
+     *      'error' => 0,         // INT 0 = no error
+     *  ]
+     *
+     * @param array $option
+     * @return array
+     */
+    public function readData($option): array
+    {
+        $questionMap = [];
+        $answerCounts = [];
+
+        $formId = $option['formId'];
+
+        $questions = $this->formsService->getQuestions($formId);
+        foreach ($questions as $question) {
+            $questionMap[$question['id']] = $question['text'];
+        }
+
+        $submissions = $this->submissionService->getSubmissions($formId);
+        foreach ($submissions as $submission) {
+            foreach ($submission['answers'] as $answer) {
+                $questionText = $questionMap[$answer['questionId']];
+                $key = $questionText . '|' . $answer['text'];
+                if (isset($answerCounts[$key])) {
+                    $answerCounts[$key]++;
+                } else {
+                    $answerCounts[$key] = 1;
+                }
+            }
+        }
+
+        foreach ($answerCounts as $key => $count) {
+            list($questionText, $text) = explode('|', $key);
+            $results[] = [$questionText, $text, $count];
+        }
+
+        // Sort by questionText ascending and count descending
+        usort($results, function ($a, $b) {
+            if ($a[0] === $b[0]) {
+                return $b[2] <=> $a[2];
+            }
+            return $a[0] <=> $b[0];
+        });
+
+        $header = array();
+        $header[0] = $this->l10n->t('Question');
+        $header[1] = $this->l10n->t('Answer');
+        $header[2] = $this->l10n->t('Count');
+
+        return [
+            'header' => $header,
+            'dimensions' => array_slice($header, 0, count($header) - 1),
+            'data' => $results,
+            //'rawdata' => $data,
+            'error' => 0,
+        ];
+    }
+}
