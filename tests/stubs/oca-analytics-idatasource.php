38646cb24b63dfe385a5da362b35a24fc2492def
Rello <Rello@users.noreply.github.com>
2024-06-27 11:01:18 +0200

    Update oca-analytics-idatasource.php
    Signed-off-by: Rello <Rello@users.noreply.github.com>

--- tests/stubs/oca-analytics-idatasource.php
+++ tests/stubs/oca-analytics-idatasource.php
@@ -2,15 +2,17 @@
 
 // reference for: use OCA\Analytics\Datasource\DatasourceEvent;
 namespace OCA\Analytics\Datasource {
-    class DatasourceEvent extends \OCP\EventDispatcher\Event {
-         abstract public function registerDatasource(string $datasource): void {}
-         abstract public function getDataSources(): array {}
-    }
+	class DatasourceEvent extends \OCP\EventDispatcher\Event {
+		abstract public function registerDatasource(string $datasource): void {
+		}
+		abstract public function getDataSources(): array {
+		}
+	}
 
-    interface IDatasource {
-        public function getName(): string;
-        public function getId(): int;
-        public function getTemplate(): array;
-        public function readData($option): array;
-    }
+	interface IDatasource {
+		public function getName(): string;
+		public function getId(): int;
+		public function getTemplate(): array;
+		public function readData($option): array;
+	}
 }
