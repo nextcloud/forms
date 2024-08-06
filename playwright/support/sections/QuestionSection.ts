6c34b2ca5be625d7efff0b6ac1cf2ced5708379c
Ferdinand Thiessen <opensource@fthiessen.de>
2024-08-06 21:10:30 +0200

    test: Add e2e test for IME input
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ playwright/support/sections/QuestionSection.ts
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ferdinand Thiessen <opensource@fthiessen.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type { Locator, Page } from '@playwright/test'
+
+export class QuestionSection {
+	public readonly titleInput: Locator
+	public readonly newAnswerInput: Locator
+	public readonly answerInputs: Locator
+
+	// eslint-disable-next-line no-useless-constructor
+	constructor(
+		public readonly page: Page,
+		public readonly section: Locator,
+	) {
+		this.titleInput = this.section.getByRole('textbox', { name: /title of/i })
+		this.newAnswerInput = this.section.getByRole('textbox', {
+			name: 'Add a new answer',
+		})
+		this.answerInputs = this.section.getByRole('textbox', {
+			name: /Answer number \d+/i,
+		})
+	}
+
+	async fillTitle(title: string): Promise<void> {
+		await this.titleInput.fill(title)
+	}
+}
