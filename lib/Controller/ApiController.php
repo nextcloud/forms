82d0374b813ebfb69e40ac7fec2b2ed06c599aa1
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2024-12-31 12:08:51 +0100

    patch for AllowEdit for Forms5
    Signed-off-by: Timotheus Pokorra <timotheus.pokorra@solidcharity.com>

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -172,6 +172,7 @@ class ApiController extends OCSController {
 				'showToAllUsers' => false,
 			]);
 			$form->setSubmitMultiple(false);
+			$form->setAllowEdit(false);
 			$form->setShowExpiration(false);
 			$form->setExpires(0);
 			$form->setIsAnonymous(false);
@@ -1294,7 +1295,7 @@ class ApiController extends OCSController {
 				continue;
 			}
 
-			$this->storeAnswersForQuestion($form, $submission->getId(), $questions[$questionIndex], $answerArray);
+			$this->storeAnswersForQuestion($form, $submission->getId(), $questions[$questionIndex], $answerArray, false);
 		}
 
 		$this->formMapper->update($form);
@@ -1307,6 +1308,87 @@ class ApiController extends OCSController {
 		}
 
 		return new DataResponse(null, Http::STATUS_CREATED);
+	}
+
+	/**
+	 * Update an existing submission
+	 *
+	 * @param int $formId the form id
+	 * @param int $submissionId the submission id
+	 * @param array<string, list<string>> $answers [question_id => arrayOfString]
+	 * @param string $shareHash public share-hash -> Necessary to submit on public link-shares.
+	 * @return DataResponse<Http::STATUS_OK, int, array{}>
+	 * @throws OCSBadRequestException Can only update submission if AllowEdit is set and the answers are valid
+	 * @throws OCSForbiddenException Can only update your own submission
+	 *
+	 * 200: the id of the updated submission
+	 */
+	#[CORS()]
+	#[NoAdminRequired()]
+	#[NoCSRFRequired()]
+	#[PublicPage()]
+	#[ApiRoute(verb: 'PUT', url: '/api/v3/forms/{formId}/submissions/{submissionId}')]
+	public function updateSubmission(int $formId, int $submissionId, array $answers, string $shareHash = ''): DataResponse {
+		$this->logger->debug('Updating submission: formId: {formId}, answers: {answers}, shareHash: {shareHash}', [
+			'formId' => $formId,
+			'answers' => $answers,
+			'shareHash' => $shareHash,
+		]);
+
+		$form = $this->loadFormForSubmission($formId, $shareHash);
+
+		if (!$form->getAllowEdit()) {
+			throw new OCSBadRequestException('Can only update if AllowEdit is set');
+		}
+
+		$questions = $this->formsService->getQuestions($formId);
+		// Is the submission valid
+		$isSubmissionValid = $this->submissionService->validateSubmission($questions, $answers, $form->getOwnerId());
+		if (is_string($isSubmissionValid)) {
+			throw new OCSBadRequestException($isSubmissionValid);
+		}
+		if ($isSubmissionValid === false) {
+			throw new OCSBadRequestException('At least one submitted answer is not valid');
+		}
+
+		// get existing submission of this user
+		try {
+			$submission = $this->submissionMapper->findByFormAndUser($form->getId(), $this->currentUser->getUID());
+		} catch (DoesNotExistException $e) {
+			throw new OCSBadRequestException('Cannot update a non existing submission');
+		}
+
+		if ($submissionId != $submission->getId()) {
+			throw new OCSForbiddenException('Can only update your own submissions');
+		}
+
+		$submission->setTimestamp(time());
+		$this->submissionMapper->update($submission);
+
+		if (empty($answers)) {
+			// Clear Answers
+			foreach ($questions as $question) {
+				$this->storeAnswersForQuestion($form, $submission->getId(), $question, [''], true);
+			}
+		} else {
+			// Process Answers
+			foreach ($answers as $questionId => $answerArray) {
+				// Search corresponding Question, skip processing if not found
+				$questionIndex = array_search($questionId, array_column($questions, 'id'));
+				if ($questionIndex === false) {
+					continue;
+				}
+
+				$question = $questions[$questionIndex];
+
+				$this->storeAnswersForQuestion($form, $submission->getId(), $question, $answerArray, true);
+			}
+		}
+
+		//Create Activity
+		$this->formsService->notifyNewSubmission($form, $submission);
+
+		return new DataResponse($submissionId);
 	}
 
 	/**
@@ -1522,14 +1604,23 @@ class ApiController extends OCSController {
 	// private functions
 
 	/**
-	 * Insert answers for a question
+	 * Insert or update answers for a question
 	 *
 	 * @param Form $form
 	 * @param int $submissionId
 	 * @param array $question
 	 * @param string[]|array<array{uploadedFileId: string, uploadedFileName: string}> $answerArray
+	 * @param bool $update
 	 */
-	private function storeAnswersForQuestion(Form $form, $submissionId, array $question, array $answerArray): void {
+	private function storeAnswersForQuestion(Form $form, int $submissionId, array $question, array $answerArray, bool $update): void {
+		// get stored answers for this question
+		$storedAnswers = [];
+		if ($update) {
+			$storedAnswers = $this->answerMapper->findBySubmissionAndQuestion($submissionId, $question['id']);
+		}
+
+		$newAnswerTexts = [];
+		
 		foreach ($answerArray as $answer) {
 			$answerEntity = new Answer();
 			$answerEntity->setSubmissionId($submissionId);
@@ -1546,6 +1637,33 @@ class ApiController extends OCSController {
 				} elseif (!empty($question['extraSettings']['allowOtherAnswer']) && strpos($answer, Constants::QUESTION_EXTRASETTINGS_OTHER_PREFIX) === 0) {
 					$answerText = str_replace(Constants::QUESTION_EXTRASETTINGS_OTHER_PREFIX, '', $answer);
 				}
+
+				if (!array_key_exists($question['id'], $newAnswerTexts)) {
+					$newAnswerTexts[$question['id']] = [];
+				}
+				$newAnswerTexts[$question['id']][] = $answerText;
+
+				// has this answer already been stored?
+				$foundAnswer = false;
+				foreach ($storedAnswers as $storedAnswer) {
+					if ($storedAnswer->getText() == $answerText) {
+						// nothing to be changed
+						$foundAnswer = true;
+						break;
+					}
+				}
+				if (!$foundAnswer) {
+					if ($answerText === '') {
+						continue;
+					}
+					// need to add answer
+					$answerEntity = new Answer();
+					$answerEntity->setSubmissionId($submissionId);
+					$answerEntity->setQuestionId($question['id']);
+					$answerEntity->setText($answerText);
+					$this->answerMapper->insert($answerEntity);
+				}
+
 			} elseif ($question['type'] === Constants::ANSWER_TYPE_FILE) {
 				$uploadedFile = $this->uploadedFileMapper->getByUploadedFileId($answer['uploadedFileId']);
 				$answerEntity->setFileId($uploadedFile->getFileId());
@@ -1565,18 +1683,41 @@ class ApiController extends OCSController {
 				$file->move($folder->getPath() . '/' . $name);
 
 				$answerText = $name;
+
+				$answerEntity->setText($answerText);
+				$this->answerMapper->insert($answerEntity);
 			} else {
 				$answerText = $answer; // Not a multiple-question, answerText is given answer
+
+				if (!empty($storedAnswers)) {
+					$answerEntity = $storedAnswers[0];
+					$answerEntity->setText($answerText);
+					$this->answerMapper->update($answerEntity);
+				} else {
+					if ($answerText === '') {
+						continue;
+					}
+					$answerEntity = new Answer();
+					$answerEntity->setSubmissionId($submissionId);
+					$answerEntity->setQuestionId($question['id']);
+					$answerEntity->setText($answerText);
+					$this->answerMapper->insert($answerEntity);
+				}
 			}
 
-			if ($answerText === '') {
-				continue;
-			}
-
-			$answerEntity->setText($answerText);
-			$this->answerMapper->insert($answerEntity);
 			if ($uploadedFile) {
 				$this->uploadedFileMapper->delete($uploadedFile);
+			}
+		}
+
+		if (in_array($question['type'], Constants::ANSWER_TYPES_PREDEFINED)) {
+			// drop all answers that are not in new set of answers
+			foreach ($storedAnswers as $storedAnswer) {
+				$questionId = $storedAnswer->getQuestionId();
+
+				if (empty($newAnswerTexts[$questionId]) || !in_array($storedAnswer->getText(), $newAnswerTexts[$questionId])) {
+					$this->answerMapper->delete($storedAnswer);
+				}
 			}
 		}
 	}
