12750e4a4fd33567f3866af2a5b1f6a407b5c361
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2025-01-04 01:02:34 +0100

    add unit test testUpdateSubmission_answers
    Signed-off-by: Timotheus Pokorra <timotheus.pokorra@solidcharity.com>

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -1346,6 +1346,10 @@ class ApiController extends OCSController {
 			throw new OCSBadRequestException('Can only update if AllowEdit is set');
 		}
 
+		if ($submissionId != $submission->getId()) {
+			throw new OCSBadRequestException('Can only update your own submissions');
+		}
+
 		$submission->setTimestamp(time());
 		$this->submissionMapper->update($submission);
 
@@ -1596,7 +1600,7 @@ class ApiController extends OCSController {
 	 * @param string[]|array<array{uploadedFileId: string, uploadedFileName: string}> $answerArray
 	 * @param bool $update
 	 */
-	private function storeAnswersForQuestion(Form $form, $submissionId, array $question, array $answerArray, bool $update): void {
+	private function storeAnswersForQuestion(Form $form, int $submissionId, array $question, array $answerArray, bool $update): void {
 		// get stored answers for this question
 		$storedAnswers = [];
 		if ($update) {
