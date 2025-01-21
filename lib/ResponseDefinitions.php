53e1d77cb15719a4b66ed8161853878a54159d44
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2025-01-21 23:31:48 +0100

    another fix for FormsForm
    Signed-off-by: Timotheus Pokorra <timotheus.pokorra@solidcharity.com>

--- lib/ResponseDefinitions.php
+++ lib/ResponseDefinitions.php
@@ -120,7 +120,7 @@ namespace OCA\Forms;
  *   shares: list<FormsShare>,
  *   submissionCount?: int,
  *   submissionMessage: ?string,
- *   answers?: non-empty-array<array-key, mixed>,
+ *   answers?: list<FormsAnswer>,
  *   newSubmission?: bool,
  *   submissionId?: int,
  * }
