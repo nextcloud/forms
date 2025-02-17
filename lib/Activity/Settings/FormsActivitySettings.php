b15e0e6893d30ed977ae77088a46001365be55ea
Christian Hartmann <chris-hartmann@gmx.de>
2025-02-17 10:54:11 +0100

    Move to min version 29 and update dependencies
    Signed-off-by: Christian Hartmann <chris-hartmann@gmx.de>

--- lib/Activity/Settings/FormsActivitySettings.php
+++ lib/Activity/Settings/FormsActivitySettings.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright Copyright (c) 2021 Jonas Rittershofer <jotoeri@users.noreply.github.com>
  *
--- lib/BackgroundJob/SyncSubmissionsWithLinkedFileJob.php
+++ lib/BackgroundJob/SyncSubmissionsWithLinkedFileJob.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright Copyright (c) 2024 Andrii Ilkiv <ailkiv@users.noreply.github.com>
  *
