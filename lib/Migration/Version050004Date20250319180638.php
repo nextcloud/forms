29979929b2056ca6da9d76ca907193fb1693afa5
Christian Hartmann <chris-hartmann@gmx.de>
2025-03-19 18:34:25 +0100

    Fix(migration): Remove parameter from setPrimaryKey method
    Signed-off-by: Christian Hartmann <chris-hartmann@gmx.de>

---
+++ lib/Migration/Version050004Date20250319180638.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Forms\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * FIXME Auto-generated migration step: Please modify to your needs!
+ */
+class Version050004Date20250319180638 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if ($schema->hasTable('forms_v2_uploaded_files')) {
+			$table = $schema->getTable('forms_v2_uploaded_files');
+			$table->dropPrimaryKey();
+			$table->setPrimaryKey(['id'], 'forms_upload_files_id');
+		}
+
+		return $schema;
+	}
+}
