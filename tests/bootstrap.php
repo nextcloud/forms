04d11915e231768b198109093df1ff1e2ac79486
Côme Chilliet <come.chilliet@nextcloud.com>
2025-05-26 14:17:31 +0200

    chore(tests): Cleanup bootstrap.php to be forward-compatible
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2017 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
+use OCP\App\IAppManager;
+use OCP\Server;
 
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
 require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
-\OC_App::loadApp('forms');
-
-\OC_Hook::clear();
+Server::get(IAppManager::class)->loadApp('forms');
