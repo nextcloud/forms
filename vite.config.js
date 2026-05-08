cf0d1d46cc7f4e9596bdf3430f30a64c58faa173
Christian Hartmann <chris-hartmann@gmx.de>
2026-05-08 11:56:03 +0200

    feat: migrate to TypeScript and update dependencies
    Signed-off-by: Christian Hartmann <chris-hartmann@gmx.de>

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,9 @@ import { join, resolve } from 'path'
 export default createAppConfig(
 	{
 		emptyContent: resolve(join('src', 'emptyContent.js')),
-		main: resolve(join('src', 'main.js')),
-		submit: resolve(join('src', 'submit.js')),
-		settings: resolve(join('src', 'settings.js')),
+		main: resolve(join('src', 'main.ts')),
+		submit: resolve(join('src', 'submit.ts')),
+		settings: resolve(join('src', 'settings.ts')),
 	},
 	{
 		config: {
