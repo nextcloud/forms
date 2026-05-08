2028490f586ceca8329f05ca7a5b86efa93a611e
Christian Hartmann <chris-hartmann@gmx.de>
2026-05-08 11:56:03 +0200

    feat: migrate to TypeScript and update dependencies
    Signed-off-by: Christian Hartmann <chris-hartmann@gmx.de>

---
+++ src/submit.ts
@@ -0,0 +1,19 @@
+/**
+ * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { translate, translatePlural } from '@nextcloud/l10n'
+import { createApp } from 'vue'
+import FormsSubmitRoot from './FormsSubmit.vue'
+
+import 'vite/modulepreload-polyfill'
+
+const app = createApp(FormsSubmitRoot)
+
+app.config.globalProperties.t = translate
+app.config.globalProperties.n = translatePlural
+
+app.mount('#content')
+
+export default app
