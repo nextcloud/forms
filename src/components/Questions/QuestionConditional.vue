dd4dd43b008eb4f684224bb8745c3ad7f317d15b
TimedIn <git@timedin.net>
2026-05-30 00:11:24 +0200

    fix: selectors for linear scale

--- src/components/Questions/QuestionConditional.vue
+++ src/components/Questions/QuestionConditional.vue
@@ -1043,8 +1043,9 @@ export default {
 				return branch.conditions.some((c) => {
 					if (c.type === 'value_equals') {
 						return numValue === parseFloat(c.value)
-					}
-					if (c.type === 'value_range') {
+					} else if (c.type === 'value_not_equals') {
+						return numValue !== parseFloat(c.value)
+					} else if (c.type === 'value_range') {
 						const min = c.min ?? Number.MIN_SAFE_INTEGER
 						const max = c.max ?? Number.MAX_SAFE_INTEGER
 						return numValue >= min && numValue <= max
