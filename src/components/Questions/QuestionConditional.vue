8fd6faea6226c1915efa873e453f914b91be2854
TimedIn <git@timedin.net>
2026-05-29 23:47:42 +0200

    feat: add ranking-question to conditional
    Signed-off-by: TimedIn <git@timedin.net>

--- src/components/Questions/QuestionConditional.vue
+++ src/components/Questions/QuestionConditional.vue
@@ -308,15 +308,6 @@
 </template>
 
 <script>
-import axios from '@nextcloud/axios'
-import { showError } from '@nextcloud/dialogs'
-import { t } from '@nextcloud/l10n'
-import { generateOcsUrl } from '@nextcloud/router'
-import { defineComponent, ref } from 'vue'
-import { VueDraggable as Draggable } from 'vue-draggable-plus'
-import NcActionButton from '@nextcloud/vue/components/NcActionButton'
-import NcActions from '@nextcloud/vue/components/NcActions'
-import NcButton from '@nextcloud/vue/components/NcButton'
 // Icons
 import IconPlus from '@material-symbols/svg-400/outlined/add.svg?raw'
 import IconArrowDownDropCircleOutline from '@material-symbols/svg-400/outlined/arrow_drop_down_circle.svg?raw'
@@ -331,6 +322,16 @@ import IconRadioboxMarked from '@material-symbols/svg-400/outlined/radio_button_
 import IconClockOutline from '@material-symbols/svg-400/outlined/schedule.svg?raw'
 import IconTextShort from '@material-symbols/svg-400/outlined/short_text.svg?raw'
 import IconTextLong from '@material-symbols/svg-400/outlined/subject.svg?raw'
+import IconSwapVertical from '@material-symbols/svg-400/outlined/swap_vert.svg?raw'
+import axios from '@nextcloud/axios'
+import { showError } from '@nextcloud/dialogs'
+import { t } from '@nextcloud/l10n'
+import { generateOcsUrl } from '@nextcloud/router'
+import { defineComponent, ref } from 'vue'
+import { VueDraggable as Draggable } from 'vue-draggable-plus'
+import NcActionButton from '@nextcloud/vue/components/NcActionButton'
+import NcActions from '@nextcloud/vue/components/NcActions'
+import NcButton from '@nextcloud/vue/components/NcButton'
 import NcIconSvgWrapper from '@nextcloud/vue/components/NcIconSvgWrapper'
 import BranchConditionEditor from './BranchConditionEditor.vue'
 import Question from './Question.vue'
@@ -342,6 +343,7 @@ import QuestionFile from './QuestionFile.vue'
 import QuestionLinearScale from './QuestionLinearScale.vue'
 import QuestionLong from './QuestionLong.vue'
 import QuestionMultiple from './QuestionMultiple.vue'
+import QuestionRanking from './QuestionRanking.vue'
 import QuestionShort from './QuestionShort.vue'
 import {
 	QUESTION_EMITS,
@@ -363,6 +365,7 @@ const QUESTION_COMPONENTS = {
 	linearscale: QuestionLinearScale,
 	color: QuestionColor,
 	file: QuestionFile,
+	ranking: QuestionRanking,
 }
 
 export default defineComponent({
@@ -385,6 +388,7 @@ export default defineComponent({
 		QuestionLong,
 		QuestionMultiple,
 		QuestionShort,
+		QuestionRanking,
 	},
 
 	props: QUESTION_PROPS,
@@ -495,6 +499,11 @@ export default defineComponent({
 				},
 				{ type: 'color', label: t('forms', 'Color'), icon: IconPalette },
 				{ type: 'file', label: t('forms', 'File'), icon: IconFile },
+				{
+					type: 'ranking',
+					label: t('forms', 'Ranking'),
+					icon: IconSwapVertical,
+				},
 			]
 		},
 
@@ -601,6 +610,7 @@ export default defineComponent({
 				linearscale: 'QuestionLinearScale',
 				color: 'QuestionColor',
 				file: 'QuestionFile',
+				ranking: 'QuestionRanking',
 			}
 			return componentMap[type] || null
 		},
@@ -724,6 +734,12 @@ export default defineComponent({
 
 				file: {
 					titlePlaceholder: t('forms', 'File question title'),
+					warningInvalid: t('forms', 'This question needs a title!'),
+					validate: () => true,
+				},
+
+				ranking: {
+					titlePlaceholder: t('forms', 'Ranking question title'),
 					warningInvalid: t('forms', 'This question needs a title!'),
 					validate: () => true,
 				},
