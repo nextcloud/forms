f67613cd02976c852bbcc684261c3833d5b77e31
Peter Ringelmann <peter.ringelmann@nextcloud.com>
2026-06-08 15:00:47 +0200

    fix: save option reorder on drop instead of during drag
    Signed-off-by: Peter Ringelmann <peter.ringelmann@nextcloud.com>

--- src/components/Questions/QuestionRanking.vue
+++ src/components/Questions/QuestionRanking.vue
@@ -141,7 +141,7 @@
 				handle=".option__drag-handle"
 				invertSwap
 				target=".sort-target"
-				@change="saveOptionsOrder('choice')"
+				@update="saveOptionsOrder('choice')"
 				@start="onDragStart"
 				@end="onDragEnd">
 				<TransitionGroup
